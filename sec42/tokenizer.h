347eb21f6e439365219a4a3dd07f37fe11c2395c
Ryohei Fushimi <ryohei@Ryohei-no-MacBook-Air.local>
2013-10-10 15:14:54 +0900

    add 字句解析器

---
+++ sec42/tokenizer.h
@@ -0,0 +1,60 @@
+// tokenizer.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+typedef enum{
+  TOK_INT,
+  TOK_BREAK,
+  TOK_CONTINUE,
+  TOK_ELSE,
+  TOK_IF,
+  TOK_RETURN,
+  TOK_WHILE,
+  TOK_INT_LITERAL,
+  TOK_ID,
+  TOK_EOF,
+  TOK_LPAREN,
+  TOK_RPAREN,
+  TOK_LBRACE,
+  TOK_RBRACE,
+  TOK_MUL,
+  TOK_PLUS,
+  TOK_MINUS,
+  TOK_DIV,
+  TOK_REM,
+  TOK_BANG,
+  TOK_LT,
+  TOK_GT,
+  TOK_LE,
+  TOK_GE,
+  TOK_EQ,
+  TOK_NEQ,
+  TOK_SEMICOLON,
+  TOK_ASSIGN,
+  TOK_COMMA,
+} token_kind_t;
+
+typedef struct token{
+  token_kind_t kind;
+  int ival;
+  char name[1024];
+} token;
+
+typedef struct tokenizer{
+  token tok;
+  FILE *fp;
+  int c;
+  int line;
+  int num;
+} *tokenizer_t;
+
+void syntax_error(tokenizer_t t);
+tokenizer_t tokenize(tokenizer_t t);
+tokenizer_t mk_tokenizer(char *filename);
+token cur_tok(tokenizer_t t);
+token next_tok(tokenizer_t t);
+void output_token(tokenizer_t t);
+int next_char(tokenizer_t t);
---
+++ sec42/tokenizer_test/04.res
@@ -0,0 +1,39 @@
+1:TOK_ID (This)
+1:TOK_ID (is)
+1:TOK_ID (a)
+1:TOK_ID (fuzzying)
+1:TOK_ID (test)
+1:TOK_ID (of)
+1:TOK_ID (tokenizers)
+2:TOK_ID (If)
+2:TOK_ID (your)
+2:TOK_ID (answer)
+2:TOK_ID (does)
+2:TOK_ID (not)
+2:TOK_ID (match)
+2:TOK_ID (the)
+2:TOK_ID (answer)
+2:TOK_COMMA
+2:TOK_ID (it)
+2:TOK_ID (is)
+2:TOK_ID (possible)
+2:TOK_ID (that)
+2:TOK_ID (the)
+2:TOK_ID (bug)
+2:TOK_ID (is)
+2:TOK_ID (in)
+2:TOK_ID (my)
+2:TOK_ID (program)
+2:TOK_COMMA
+2:TOK_ID (not)
+2:TOK_ID (yours)
+3:TOK_GT
+3:TOK_ID (buVF)
+4:TOK_INT_LITERAL (6)
+4:TOK_ID (xfCPV09I6u6C6J8)
+4:TOK_ID (DZmWVutJ2w)
+4:TOK_COMMA
+4:TOK_INT_LITERAL (9)
+syntax error :4:7
+
+      ^
